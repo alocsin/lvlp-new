233d36c1a58458e8f46c09f96287d98fd1ffc247
alocsin <41352004+alocsin@users.noreply.github.com>
2018-08-08 00:41:10 +0800

    push all files to github

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+$('.filters-list a').on('click', function() {
+  var filter = $(this).data('filter')
+
+  console.log(filter)
+
+  $('.all-projects').addClass('hidden')
+  $(filter).removeClass('hidden')
+
+  $('.all-projects').fadeOut(1000)
+  $(filter).delay(1000).fadeIn(1000)
+
+  // this will add a class name of selected to the current filter link that we clicked on
+  $('.filters-list a').removeClass('selected')
+  $(this).addClass('selected')
+
+  // this blocks link from following the href
+  return false
+})
+
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out-up',
+    inDuration: 1500,
+    outDuration: 1500,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: true,
+    timeoutCountdown: 0,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+});
